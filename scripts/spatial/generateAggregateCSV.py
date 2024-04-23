c07d4fec33373bdfc2d3c86e5c5c161ebdea6bc0
xies4 <xies4@nih.gov>
2024-04-23 00:10:31 -0400

    Update generateAggregateCSV.py to include cloupe file and spatial folder paths.

--- scripts/spatial/generateAggregateCSV.py
+++ scripts/spatial/generateAggregateCSV.py
@@ -1,15 +1,20 @@
 import glob, sys
+import re
+import os
 
 def main(path):
     files = glob.glob('*/outs/molecule_info.h5')
     aggrFile = open('AggregatedDatasets.csv', 'w')
 
     #aggrFile = open('/Users/chenv3/Downloads/aggregate_samples.csv', 'w')
-
-    aggrFile.write('sample_id,molecule_h5\n')
+    # https://www.10xgenomics.com/support/software/space-ranger/latest/analysis/running-pipelines/aggr-aggr
+    aggrFile.write('library_id,molecule_h5,cloupe_file,spatial_folder\n')
     files.sort()
     for i in files:
-        aggrFile.write('%s,%s/%s\n' % (i.split('/')[0], path, i))
+        cloupe_file = os.path.join(path, re.sub("outs/molecule_info.h5", "outs/cloupe.cloupe", i)) 
+        spatial_folder = os.path.join(path, re.sub("outs/molecule_info.h5", "outs/spatial", i))
+        print([cloupe_file, spatial_folder])
+        aggrFile.write('%s,%s/%s,%s,%s\n' % (i.split('/')[0], path, i, cloupe_file, spatial_folder))
     aggrFile.close()
 
 if __name__ == "__main__":

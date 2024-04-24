92ce39e7bef11858e63747bb2faf376bd8d3eba2
xies4 <xies4@nih.gov>
2024-04-24 10:32:54 -0400

    rm sc_qc.R

--- scripts/rna/sc_qc.R
+++
@@ -1,9 +0,0 @@
-library(Seurat)
-library(Matrix)
-library(MASS)
-library(dplyr)
-library(Biostrings)
-library(reshape2)
-
-seurat_obj <- CreateSeuratObject(counts = Read10X(data.dir = path2cnt_gene_BR1_1), project = "MAS_BR1_1")
-

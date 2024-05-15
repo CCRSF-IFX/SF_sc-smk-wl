912d2957c05d02dabdc17f61c72f67e819cc52a3
xies4 <xies4@nih.gov>
2024-05-15 11:33:18 -0400

    rm test_data/

--- test_data/test4slurm_config/test.sh
+++
@@ -1,3 +0,0 @@
-# A minimal demo to test the configuration for slurm job submission 
-snakemake -s Snakefile --profile ../../profile/slurm/ -e cluster-generic 
-rm -rf jobs/ .snakemake/ results/ slurm-* 

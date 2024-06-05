46756e4f5d0046b37b5a0ab074465ab1c1ad041e
xies4 <xies4@nih.gov>
2024-06-05 17:39:26 -0400

    Add config/submit.sh

---
+++ config/submit_ext.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=norm
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --mem=4g
+#SBATCH --mail-user=$USER@nih.gov
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time=96:00:00
+#SBATCH --no-requeue
+#SBATCH --job-name=submit.sh
+
+module load singularity 
+snakemake --jobname 's.{jobid}.{rulename}' --profile workflow/profile/slurm/  -e cluster-generic > snakemake.log 2>&1 

d1e0fc4014be6c104269fa87165c89a36555d1c7
xies4 <xies4@nih.gov>
2024-08-12 14:43:29 -0400

    Fix a typo

--- scripts/curio/generate_sample_sheet.py
+++ scripts/curio/generate_sample_sheet.py
@@ -18,13 +18,13 @@ analysis_folder = args[2]
 with open(args[1], "r") as fcsv:
     header = next(fcsv)
     header = header.rstrip()
-    print(f"{header},fastq_1,fastq2")
+    print(f"{header},fastq_1,fastq_2")
     for line in fcsv:
         ele = line.rstrip().split(",")
         sample = ele[0]
         with open(f"{sample}.csv", "w+") as fout:
             fq1 = os.path.join(analysis_folder, f"fastq4curio/{sample}_R1.fastq.gz")
             fq2 = os.path.join(analysis_folder, f"fastq4curio/{sample}_R2.fastq.gz")
-            fout.write(f"{header},fastq_1,fastq2\n")
+            fout.write(f"{header},fastq_1,fastq_2\n")
             fout.write(f"{line.rstrip()},{fq1},{fq2}\n")
         print(f"{header},{fq1},{fq2}")

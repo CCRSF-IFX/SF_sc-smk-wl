5084fbdb8981521563c0e08f13fb702666c5aee1
xies4 <xies4@nih.gov>
2026-07-10 23:11:16 -0400

    config/reference.py

--- config/program.py
+++ config/program.py
@@ -33,6 +33,9 @@ tenx_cloud_token_path = "/mnt/ccrsf-ifx/RefGenomes/SingleCell_REF/10X/CLI/txg"
 
 # v4.1.2
 nf_pixelator_path = "/mnt/ccrsf-ifx/Software/tools/nf-pixelator/v4.1.2/"
+# https://quay.io/repository/pixelgen-technologies/pixelator?tab=tags
+# https://github.com/PixelgenTechnologies/pixelator/releases
+# pixelator_container = "quay.io/pixelgen-technologies/pixelator:0.29.0"
 
 parsebio_split_pipe = "module purge; module load miniconda; conda activate /mnt/ccrsf-ifx/Software/tools/Parse_Pipeline/condaenv_spipe_v1.6.1/; split-pipe "
 bdrhapsody = "/mnt/ccrsf-ifx/Software/tools/BD_Rhapsody/rhapsodyPipeline-3.0/rhapsody"
